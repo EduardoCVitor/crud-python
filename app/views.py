c5f5f518c9fa2c45b434ae7e3cd0b76a5efe8ac1
EduardoCVitor <eduardovitor@gmail.com>
2022-12-21 10:07:11 -0300

    Testando o servidor

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse # Importa do django a função para responder um chamado de HTTP
 
 # Create your views here.
+
+def home(request):                      # Define a função Home que ao ser chamada
+    return HttpResponse("Hello World")  # A resposta do HTTP retorna um Hello World
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app' # Adicionando o nome da aplicação
 ]
 
 MIDDLEWARE = [
