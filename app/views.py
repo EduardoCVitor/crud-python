97360c92a66c287254a19a8ef817d2c19478eff1
EduardoCVitor <eduardovitor@gmail.com>
2022-12-22 12:45:55 -0300

    Finalizando aula3

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from app.forms import CarrosForm
 
 # Create your views here.
 
 def home(request):                      # Define a função Home que ao ser chamada
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
 def form(request):                      # Define a função Home que ao ser chamada
-    return render(request, "form.html")
+    data = {}
+    data['form'] = CarrosForm()
+    return render(request, 'form.html', data)
+ 
